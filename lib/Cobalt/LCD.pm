0700a6a91a4681cf0eae0ac346b99427e2e4f7be
Jeff Walter <jeff@404ster.com>
2005-07-21 15:41:58 +0000

    Import of JEFFW/Cobalt-LCD-0.02 from CPAN.
    gitpan-cpan-distribution: Cobalt-LCD
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         JEFFW/Cobalt-LCD-0.02.tar.gz
    gitpan-cpan-author:       JEFFW
    gitpan-cpan-maturity:     released

--- lib/Cobalt/LCD.pm
+++ lib/Cobalt/LCD.pm
@@ -1,21 +1,15 @@
 package Cobalt::LCD;
 
-use 5.008002;
+use 5.006000;
 use strict;
 use warnings;
+use Time::HiRes qw(time usleep);
 
 require Exporter;
 use AutoLoader qw(AUTOLOAD);
 
 our @ISA = qw(Exporter);
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Cobalt::LCD ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
 our %EXPORT_TAGS = ( 'all' => [ qw(
 	
 ) ] );
@@ -26,62 +20,216 @@ our @EXPORT = qw(
 	
 );
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
+sub new {
+    my $proto = shift;
+    
+    my $self = {
+        '_SYSTEM_TYPE'      => '',
+        
+        '_PROG_GETIP'       => '/sbin/lcd-getip', 
+        '_PROG_WRITE'       => '/sbin/lcd-write', 
+        '_PROG_FLASH'       => '/sbin/lcd-flash', 
+        '_PROG_READBUTTON'  => '/sbin/readbutton',
+        '_PROC_SYSTYPE'     => '/proc/cobalt/systype',
+        
+        '_DEBOUNCE'         => 0,
+        '_DEBOUNCE_TIMEOUT' => 0.5,
+        
+        '_USLEEP'           => 1000,
+        
+        'BUTTON_NONE'       => 0,
+        'BUTTON_RESET'      => 0,
+        'BUTTON_SELECT'     => 0,
+        'BUTTON_EXIT'       => 0,
+        'BUTTON_LEFT'       => 0,
+        'BUTTON_RIGHT'      => 0,
+        'BUTTON_UP'         => 0,
+        'BUTTON_DOWN'       => 0,
+    };
+    
+    bless $self, $proto;
+    
+    $self->lcd_init();
+    
+    return $self;
+}
 
-# Preloaded methods go here.
+sub lcd_init ($) {
+    my $self = shift;
+    open(PROC,'<'.$self->{_PROC_SYSTYPE}) or die "Cannot open $self->{_PROC_SYSTYPE}!\nCheck to be sure the Cobalt drivers are installed in the kernel.";
+    chomp($self->{_SYSTEM_TYPE} = lc(<PROC>));
+    close (PROC);
 
-# Autoload methods go after =cut, and are processed by the autosplit program.
+    if ($self->{_SYSTEM_TYPE} eq 'pacifica') {
+        $self->{BUTTON_NONE} = 0;
+        $self->{BUTTON_RESET} =  64512;
+        $self->{BUTTON_SELECT} = 32256;
+        $self->{BUTTON_EXIT} =   48640;
+        $self->{BUTTON_LEFT} =   64000;
+        $self->{BUTTON_RIGHT} =  56832;
+        $self->{BUTTON_UP} =     62976;
+        $self->{BUTTON_DOWN} =   60928;
+        
+    } elsif ($self->{_SYSTEM_TYPE} eq 'carmel') {
+        $self->{BUTTON_NONE} = 0;
+        $self->{BUTTON_RESET} =  64512;
+        $self->{BUTTON_SELECT} = 32256;
+        $self->{BUTTON_EXIT} =   48640;
+        $self->{BUTTON_LEFT} =   64000;
+        $self->{BUTTON_RIGHT} =  56832;
+        $self->{BUTTON_UP} =     62976;
+        $self->{BUTTON_DOWN} =   60928;
+        
+    } elsif ($self->{_SYSTEM_TYPE} eq 'monterey') {
+        $self->{BUTTON_NONE} = 0;
+        $self->{BUTTON_RESET} =  64512;
+        $self->{BUTTON_SELECT} = 32256;
+        $self->{BUTTON_EXIT} =   48640;
+        $self->{BUTTON_LEFT} =   64000;
+        $self->{BUTTON_RIGHT} =  56832;
+        $self->{BUTTON_UP} =     62976;
+        $self->{BUTTON_DOWN} =   60928;
+        
+    } elsif ($self->{_SYSTEM_TYPE} eq 'alpine') {
+        $self->{BUTTON_NONE} = 0;
+        $self->{BUTTON_RESET} =  64512;
+        $self->{BUTTON_SELECT} = 32256;
+        $self->{BUTTON_EXIT} =   48640;
+        $self->{BUTTON_LEFT} =   64000;
+        $self->{BUTTON_RIGHT} =  56832;
+        $self->{BUTTON_UP} =     62976;
+        $self->{BUTTON_DOWN} =   60928;
+        
+    } elsif ($self->{_SYSTEM_TYPE} eq 'bigbear') {
+        $self->{BUTTON_NONE} = 0;
+        $self->{BUTTON_RESET} =  64512;
+        $self->{BUTTON_SELECT} = 32256;
+        $self->{BUTTON_EXIT} =   48640;
+        $self->{BUTTON_LEFT} =   64000;
+        $self->{BUTTON_RIGHT} =  56832;
+        $self->{BUTTON_UP} =     62976;
+        $self->{BUTTON_DOWN} =   60928;
+        
+    } else {
+        die sprintf("Cannot determine the system type of the Cobalt.\n/proc/cobalt/systype reports %s.",$self->{_SYSTEM_TYPE});
+    }
+}
+
+sub write ($$$) {
+    my ($self,$line_a,$line_b) = @_;
+    
+    return system(sprintf('%s "%s" "%s"',$self->{_PROG_WRITE},$line_a,$line_b));
+}
+
+sub flash ($) {
+    my $self = shift;
+        warn "flash() is not currently supported as it locks the LCD.";    return;
+        #return system(sprintf('%s',$self->{_PROG_FLASH}));
+}
+
+sub buttonstate ($) {
+    my $self = shift;
+    
+    return system(sprintf('%s',$self->{_PROG_READBUTTON}))
+}
+
+sub waitforbutton ($$$) {
+    my ($self,$timeout_time,$maxdown_time) = @_;
+    my ($button,$start_time) = (0,0);
+    
+    while ($self->{_DEBOUNCE} > time()) {
+        usleep $self->{_USLEEP};
+    };
+    
+    $timeout_time += time();
+    
+    while (($button = $self->buttonstate()) == 0) {
+        usleep $self->{_USLEEP};
+        return ([0,0]) if ($timeout_time <= time());
+    }
+    $start_time = time();
+    $maxdown_time = 60 if (!defined($maxdown_time));
+    $maxdown_time += time();
+    
+    while ($self->buttonstate() != 0) {
+        usleep $self->{_USLEEP};
+        if ($maxdown_time <= time()) {
+            $self->{_DEBOUNCE} = time() + $self->{_DEBOUNCE_TIMEOUT};
+            return ([$button,time()-$start_time]);
+        }
+    };
+
+    return ([$button,time()-$start_time]);
+}
+
+sub getip ($$$) {
+    my ($self,$line_a,$ip) = @_;
+
+    my $iip = `$self->{_PROG_GETIP} -1 \"$line_a\" -i $ip`;
+
+    foreach (split(/\./,$iip,4)) {
+        if (($_ < 0) || ($_ > 255)) {
+            $iip = ''
+        }
+    }
+
+    return $iip;
+}
 
 1;
+
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-Cobalt::LCD - Perl extension for blah blah blah
+Cobalt::LCD - Perl extension for interacting with the sys-apps/cobalt-panel-utils on Gentoo
 
 =head1 SYNOPSIS
 
   use Cobalt::LCD;
-  blah blah blah
+
+  my $lcd = Cobalt::LCD->new();
+  
+  I'm not going to go in to detail at this time as the package is still very much in testing. If you want to play, read the source.
 
 =head1 DESCRIPTION
 
-Stub documentation for Cobalt::LCD, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
+Cobalt::LCD is used to work in a more friendly fashion with the Gentoo sys-apps/cobalt-panel-utils package.
 
 =head2 EXPORT
 
-None by default.
+Export? Do I have to pay taxes? I thought this sucker was duty free...
 
-
+No exports, works better as an object due to certain bits of state information.
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
+Provided you have the sys-apps/cobalt-panel-utils package installed, you can `man` the heck out of those utilities:
 
-If you have a mailing list set up for your module, mention it here.
+lcd-write
+lcd-swrite
+lcd-yesno
+lcd-setcursor
+lcd-getip
+lcd-flash
+readbutton
+iflink
+iflinkstatus
 
-If you have a web site set up for your module, mention it here.
+Also, checkout http://gentoo.404ster.com/ as that's where my projects are tracked.
 
 =head1 AUTHOR
 
-A. U. Thor, E<lt>illogical@rhs-inc.comE<gt>
+Jeff Walter <lt>jeffw@404ster.com<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2005 by A. U. Thor
+Copyright (C) 2005 by Jeff Walter
 
 This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.8.2 or,
+it under the same terms as Perl itself, either Perl version 5.8.4 or,
 at your option, any later version of Perl 5 you may have available.
-
 
 =cut
